74f90851e83f2564b8f790d2411e83ff62644bd8
Nikolaj Andersen <nikolaj.andersen@mynewsdesk.com>
2014-09-03 16:17:53 +0200

    change merge.rb

--- lib/commands/merge.rb
+++ lib/commands/merge.rb
@@ -2,11 +2,16 @@
 class Merge < Flower::Command
   respond_to "merge"
 
+  IMAGES = %w[
+    http://cdn.memegenerator.net/instances/500x/46317286.jpg
+    http://i.imgur.com/JISZt5A.png
+  ]
+
   def self.description
     "Merge my pull request!"
   end
 
   def self.respond(message)
-    message.say "http://cdn.memegenerator.net/instances/500x/46317286.jpg"
+    message.say IMAGES.sample
   end
 end
