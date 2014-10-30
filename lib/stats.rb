a0d6064e5dad4957d6b63c042738f99e13e08da6
Teo Ljungberg <teo@teoljungberg.com>
2014-10-30 21:36:53 +0100

    Remove leaderboard notifications

--- lib/stats.rb
+++ lib/stats.rb
@@ -11,9 +11,7 @@ class Flower::Stats
   end
 
   def self.store_leaderboard_stat(message)
-    report_change(message) do
-      store("leaderboard", {message.sender[:nick] => 1})
-    end
+    store("leaderboard", {message.sender[:nick] => 1})
   rescue
     puts "REDIS ERROR"
   end
@@ -35,37 +33,6 @@ class Flower::Stats
   end
 
   private
-
-  def self.report_change(message, &block)
-    before = sorted_leaderboard.map(&:first)
-    yield(block)
-    after = sorted_leaderboard.map(&:first)
-    if before != after
-      passed_user = before[before.index(message.sender[:nick]) - 1]
-      new_position = after.index(message.sender[:nick]) + 1
-      message.say("[Leaderboard] #{message.sender[:nick]} passed #{passed_user}, and is now number #{new_position}! #{random_insult(passed_user)}") if message.flower
-      play_random_sound
-    end
-  end
-
-  def self.play_random_sound
-    sound = ["soundfx/easy.mp3", "soundfx/rimshot.mp3", "soundfx/sadtrombone.mp3", "soundfx/yeah.mp3",
-      "soundfx/applause.mp3", "soundfx/bomb.mp3", "soundfx/suprise.m4r", "soundfx/godwillsit.m4a"].sample
-    Thread.new { SoundCommand.play_file(sound) }
-  end
-
-  def self.random_insult(nick)
-    [
-      "Get to work #{nick}.",
-      "You can do better #{nick}.",
-      "Why the sad face, #{nick}?",
-      "You are not looking too good #{nick}.",
-      "Loooser #{nick}!",
-      "You are losing it, #{nick}!",
-      "SAY SOMETHING #{nick}!?",
-      "Are you dead #{nick}?"
-    ].sample
-  end
 
   def self.sorted_leaderboard(to_date = 1.hour.from_now)
     sort_list(find("leaderboard", 1000.days.ago, to_date).total).to_a
